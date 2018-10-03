04bd22dedf385d513bb508955f1e56b1d8e2be84
nm-t <nathaliamtan@gmail.com>
2018-10-03 22:08:20 +1000

    [FEAT] Changed Winning Colors into SVG. Updated Mouth to be a div for easier manipulation.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,12 +1,5 @@
-// Approximate relative location of horse's mouth
-var HORSE_MOUTH_X_START = 0.85;
-var HORSE_MOUTH_X_END = 0.91;
-var HORSE_MOUTH_Y_START = 0.39;
-var HORSE_MOUTH_Y_END = 0.45;
-
 // List of edible items
 var FOOD_LIST = ['apple','carrot','hay'];
-
 
 document.addEventListener("DOMContentLoaded", function (event) {
     var audio = document.querySelector('audio');
@@ -17,33 +10,22 @@ document.addEventListener("DOMContentLoaded", function (event) {
     };
 });
 
-
 function changeCursor(elem,type) {
     document.getElementsByClassName('horse')[0].className = 'horse ' + type;
 }
-
-function checkMouth(clickX, clickY, imgWidth, imgHeight) {
-    var clickWidth = clickX / imgWidth;
-    var clickHeight = clickY / imgHeight;
-    return (HORSE_MOUTH_X_START <= clickWidth && clickWidth <= HORSE_MOUTH_X_END) &&
-            (HORSE_MOUTH_Y_START <= clickHeight && clickHeight <= HORSE_MOUTH_Y_END)
-}
-
 
 function checkFood(item) {
   return FOOD_LIST.includes(item);
 }
 
-
-function onHorseMouseDown(img, event) {
+function onHorseMouseDown(img) {
     var element = img.parentElement;
     var isFood = checkFood(element.classList[element.classList.length-1]);
-    var isMouth = checkMouth(event.offsetX, event.offsetY, img.width, img.height);
-    if ((isFood && isMouth) || (!isFood && !isMouth)) {
+    if (isFood) {
         element.classList.add("active");
     }
 }
 
-function onHorseMouseUp(img, event) {
+function onHorseMouseUp(img) {
     img.parentElement.classList.remove("active");
 }
