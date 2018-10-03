147a13deb802604873473b530dc92560d867f6fe
Yannik Hewlik <yannik@hewlik.de>
2018-10-03 14:12:16 +0200

    delete unused script.js and fix function for toggling active state of sidebar icons

--- public/js/main.js
+++ public/js/main.js
@@ -18,9 +18,13 @@ document.body.addEventListener("DOMContentLoaded", function (event) {
     };
 });
 
-
-function changeCursor(elem,type) {
+function changeCursor(elem, type) {
     document.getElementsByClassName('horse')[0].className = 'horse ' + type;
+    var elems = document.querySelectorAll(".icon-bar button");
+    [].forEach.call(elems, function (el) {
+        el.classList.remove("active");
+    });
+    elem.classList.add('active');
 }
 
 function checkMouth(clickX, clickY, imgWidth, imgHeight) {
