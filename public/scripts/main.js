30712082238fab840c18d3f26787f091f56cdb7e
Casey Dierking <casey@leantech.com>
2018-10-05 12:57:05 -0500

    Revert "Merge pull request #43 from adsingh14/master"
    This reverts commit cd7fd0f1631330519deeb49adbc5c188108b2082, reversing
    changes made to 9e696c0cf0ed8263be1744aa4979e82aa3c7aacb.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,34 +1,38 @@
-"use strict";
 // List of edible items
-var FOOD_LIST = ["apple", "carrot", "hay"];
+var FOOD_LIST = ['apple','carrot','hay'];
 
-document.body.addEventListener("DOMContentLoaded", function() {
-  checkIfNightTime();
+document.addEventListener("DOMContentLoaded", function(){
+    checkIfNightTime();
 });
 
-document.body.addEventListener("DOMContentLoaded", function(event) {
-  var audio = document.querySelector("audio");
-  audio.volume = 0.05;
+document.addEventListener("DOMContentLoaded", function (event) {
+    var audio = document.querySelector('audio');
+    audio.volume = 0.05;
 
-  document.onmouseover = function() {
-    audio.play();
-  };
+    document.onmouseover = function () {
+        audio.play();
+    };
 });
 
-function checkIfNightTime() {
-  var currentHours = new Date().getHours();
-  var isCurrentlyNightTime = currentHours >= 18 || currentHours <= 8;
-  var bodyElement = document.querySelector("body");
-  var isNightTimeAlreadyToggled = bodyElement.classList.contains("dark-mode");
+function checkIfNightTime()
+{
+    var currentHours = new Date().getHours();
+    var isCurrentlyNightTime = (currentHours >= 18 || currentHours <= 8);
+    var bodyElement = document.querySelector('body');
+    var isNightTimeAlreadyToggled = bodyElement.classList.contains('dark-mode');
 
-  if (isCurrentlyNightTime && !isNightTimeAlreadyToggled) {
-    bodyElement.classList.add("dark-mode");
-  } else if (!isCurrentlyNightTime && isNightTimeAlreadyToggled) {
-    bodyElement.classList.remove("dark-mode");
-  }
+    if(isCurrentlyNightTime && !isNightTimeAlreadyToggled)
+    {
+        bodyElement.classList.add("dark-mode");
+    }
+    else if(!isCurrentlyNightTime && isNightTimeAlreadyToggled)
+    {
+        bodyElement.classList.remove("dark-mode");
+    }
 }
 setInterval(checkIfNightTime, 5 * 60 * 1000);
 
+<<<<<<< HEAD
 function changeCursor(elem, type) {
   document.getElementsByClassName("horse-area")[0].className = "horse-area " + type;
   var elems = document.querySelectorAll(".icon-bar button");
@@ -36,6 +40,15 @@ function changeCursor(elem, type) {
     el.classList.remove("active");
   });
   elem.classList.add("active");
+=======
+function changeCursor(elem,type) {
+    document.getElementsByClassName('horse')[0].className = 'horse ' + type;
+    var elems = document.querySelectorAll(".icon-bar button");
+    [].forEach.call(elems, function (el) {
+        el.classList.remove("active");
+    });
+    elem.classList.add('active');
+>>>>>>> parent of cd7fd0f... Merge pull request #43 from adsingh14/master
 }
 
 function checkFood(item) {
@@ -43,17 +56,17 @@ function checkFood(item) {
 }
 
 function onHorseMouseDown(img) {
-  var element = img.parentElement;
-  var isFood = checkFood(element.classList[element.classList.length - 1]);
-  if (isFood) {
-    element.classList.add("active");
-  }
+    var element = img.parentElement;
+    var isFood = checkFood(element.classList[element.classList.length-1]);
+    if (isFood) {
+        element.classList.add("active");
+    }
 }
 
 function onHorseMouseUp(img) {
-  img.parentElement.classList.remove("active");
+    img.parentElement.classList.remove("active");
 }
 
-document.querySelector(".dark-mode-btn").addEventListener("click", function() {
-  document.querySelector("body").classList.toggle("dark-mode");
+document.querySelector('.dark-mode-btn').addEventListener('click', function(){
+    document.querySelector('body').classList.toggle('dark-mode');
 });
